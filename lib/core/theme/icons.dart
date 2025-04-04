240f2f9a72ba21b3ce8fdda12950168a7d0ffbdf
Otabek Nuriddinov <otabeknuriddinov7808@gmail.com>
2025-04-04 18:35:47 +0500

    [Otabek] Svg qilib pdp va google iconlarni qo'shildi

---
+++ lib/core/theme/icons.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_svg/svg.dart';
+
+sealed class AppIcons{
+  static SvgPicture pdp = SvgPicture.asset("assets/icons/pdp_logo.svg");
+  static SvgPicture googleBut = SvgPicture.asset("assets/icons/google_button.svg");
+}
