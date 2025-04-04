d1f0a739ac215e098a162b5ff3343063ded92a5d
Otabek Nuriddinov <otabeknuriddinov7808@gmail.com>
2025-04-04 18:22:56 +0500

    [Otabek]AppRoutes da o'zimga berilganlarini tayyor qildim

--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:pdp_quiz/core/config/routes.dart';
 
 class PdpQuiz extends StatelessWidget {
   const PdpQuiz({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      initialRoute: AppRoutes.splash,
+      routes: AppRoutes.routes,
+    );
   }
 }
---
+++ lib/core/config/routes.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:pdp_quiz/screen/confirmation.dart';
+import 'package:pdp_quiz/screen/sign_in.dart';
+import 'package:pdp_quiz/screen/sign_up.dart';
+import 'package:pdp_quiz/screen/splash_screen.dart';
+
+sealed class AppRoutes{
+  static final splash = 'splash';
+  static final signIn = 'signIn';
+  static final signUp = 'signUp';
+  static final confirmation = 'confirm';
+
+  static Map<String, Widget Function(BuildContext)> routes = <String, WidgetBuilder>{
+    AppRoutes.splash: (context)=>const SplashScreen(),
+    AppRoutes.signIn: (context)=>const SignIn(),
+    AppRoutes.signUp: (context)=>const SignUp(),
+    AppRoutes.confirmation: (context)=> const Confirmation()
+  };
+}
