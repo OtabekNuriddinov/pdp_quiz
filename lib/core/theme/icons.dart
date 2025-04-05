0641c6a0405f54a7855e8fddcdfcd7b0981046b0
Otabek Nuriddinov <otabeknuriddinov7808@gmail.com>
2025-04-05 10:03:51 +0500

    [Otabek] page 8 sal-pal to'g'irlandi

--- lib/core/theme/icons.dart
+++ lib/core/theme/icons.dart
@@ -3,4 +3,8 @@ import 'package:flutter_svg/svg.dart';
 sealed class AppIcons{
   static SvgPicture pdp = SvgPicture.asset("assets/icons/pdp_logo.svg");
   static SvgPicture googleBut = SvgPicture.asset("assets/icons/google_button.svg");
+  static SvgPicture home = SvgPicture.asset("assets/icons/google_button.svg");
+  static SvgPicture grade = SvgPicture.asset("assets/icons/google_button.svg");
+  static SvgPicture profile = SvgPicture.asset("assets/icons/google_button.svg");
+  static SvgPicture littleFlutter = SvgPicture.asset("assets/icons/little_flutter_logo.svg");
 }
--- lib/screen/app_color.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-sealed class AppColors{
-  static const white=Colors.white;
-  static const green=Color(0xFF3DFF32);
-  static const black=Colors.black;
-
-}
