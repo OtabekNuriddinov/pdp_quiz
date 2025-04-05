ecc2d1927fb18154f390639a3171e9372de2013b
Otabek Nuriddinov <otabeknuriddinov7808@gmail.com>
2025-04-05 20:17:33 +0500

    [Otabek] Sign In va Up ga animatsiya qo'shildi

--- lib/screen/sign_in/sign_in.dart
+++ lib/screen/sign_in/sign_in.dart
@@ -9,86 +9,160 @@ import '../../core/widgets/or_sign_in_with.dart';
 import '/core/theme/icons.dart';
 import '/core/theme/text_styles.dart';
 
-class SignIn extends StatelessWidget {
+class SignIn extends StatefulWidget {
   const SignIn({super.key});
+
+  @override
+  State<SignIn> createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> with TickerProviderStateMixin {
+  final int itemCount = 8;
+  late List<AnimationController> _controllers;
+  late List<Animation<Offset>> _animations;
+
+  @override
+  void initState() {
+    super.initState();
+    _controllers = List.generate(
+      itemCount,
+      (index) => AnimationController(
+        vsync: this,
+        duration: Duration(milliseconds: 500),
+      ),
+    );
+
+    _animations = _controllers.map((controller){
+      return Tween<Offset>(
+        begin: Offset(0, -1),
+        end: Offset(0, 0)
+      ).animate(controller);
+    }).toList();
+    _runStaggeredAnimations();
+  }
+
+  void _runStaggeredAnimations()async{
+    for(int i=_controllers.length-1; i>=0; i--){
+      await Future.delayed(Duration(milliseconds: 100));
+      _controllers[i].forward();
+    }
+  }
+
+  @override
+  void dispose() {
+    for(var controller in _controllers){
+      controller.dispose();
+    }
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: AppColors.white,
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: AppDimens.h40v70,
-            child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  SizedBox(
-                    width: 205,
-                    height: 65,
-                    child: AppIcons.pdp,
-                  ),
-                  SizedBox(height: 40),
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: Text("Hello,", style: AppTextStyles.helloStyle),
-                  ),
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: Text("You Are Welcome",
-                        style: AppTextStyles.welcomeStyle),
-                  ),
-                  SizedBox(height: 50),
-                  SizedBox(
-                    height: 66,
-                    child: MainTextField(
-                      label: "Phone Number",
-                      hint: "+998",
-                    ),
-                  ),
-                  AppDimens.s24,
-                  SizedBox(
-                    height: 60,
-                    width: double.infinity,
-                    child: MainElevatedButton(
-                      text: "Sign In",
-                      onTapped: (){
-                        Navigator.pushReplacementNamed(context, AppRoutes.signUp);
-                      },
-                    ),
-                  ),
-                  AppDimens.s24,
-                  OrSignInWith(),
-                  AppDimens.s24,
-                  SizedBox(
-                    width: 55,
-                    height: 55,
-                    child: Material(
-                      color: AppColors.white,
-                      elevation: 4,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(12)),
-                      child: AppIcons.googleBut,
-                    ),
-                  ),
-                  SizedBox(height: 40),
-                  RichText(
-                    text: TextSpan(
-                      children: [
-                        TextSpan(
-                            text: "Don't have an account?",
-                            style: AppTextStyles.dHAnAccount),
-                        TextSpan(
-                            text: " Sign Up",
-                            style: AppTextStyles.signInOut,
-                            recognizer: TapGestureRecognizer()..onTap = () {})
-                      ],
-                    ),
-                  )
-                ],
+      backgroundColor: AppColors.white,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: AppDimens.h40v70,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SlideTransition(
+                position: _animations[0],
+                child: SizedBox(
+                  width: 205,
+                  height: 65,
+                  child: AppIcons.pdp,
+                ),
               ),
-            
+              SizedBox(height: 40),
+
+              SlideTransition(
+                position: _animations[1],
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text("Hello,", style: AppTextStyles.helloStyle),
+                ),
+              ),
+
+              SlideTransition(
+                position: _animations[2],
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child:
+                      Text("You Are Welcome", style: AppTextStyles.welcomeStyle),
+                ),
+              ),
+              SizedBox(height: 50),
+
+              SlideTransition(
+                position: _animations[3],
+                child: SizedBox(
+                  height: 66,
+                  child: MainTextField(
+                    label: "Phone Number",
+                    hint: "+998",
+                  ),
+                ),
+              ),
+
+              AppDimens.s24,
+
+              SlideTransition(
+                position: _animations[4],
+                child: SizedBox(
+                  height: 60,
+                  width: double.infinity,
+                  child: MainElevatedButton(
+                    text: "Sign In",
+                    onTapped: () {
+                      Navigator.pushReplacementNamed(context, AppRoutes.signUp);
+                    },
+                  ),
+                ),
+              ),
+              AppDimens.s24,
+              SlideTransition(
+                position: _animations[5],
+                  child: OrSignInWith(),
+              ),
+              AppDimens.s24,
+
+              SlideTransition(
+                position: _animations[6],
+                child: SizedBox(
+                  width: 55,
+                  height: 55,
+                  child: Material(
+                    color: AppColors.white,
+                    elevation: 4,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12)),
+                    child: AppIcons.googleBut,
+                  ),
+                ),
+              ),
+              SizedBox(height: 40),
+
+              SlideTransition(
+                position: _animations[7],
+                child: RichText(
+                  text: TextSpan(
+                    children: [
+                      TextSpan(
+                          text: "Don't have an account?",
+                          style: AppTextStyles.dHAnAccount),
+                      TextSpan(
+                          text: " Sign Up",
+                          style: AppTextStyles.signInOut,
+                          recognizer: TapGestureRecognizer()..onTap = () {})
+                    ],
+                  ),
+                ),
+              )
+            ],
           ),
         ),
+      ),
     );
   }
 }
--- lib/screen/sign_up/sign_up.dart
+++ lib/screen/sign_up/sign_up.dart
@@ -17,8 +17,48 @@ class SignUp extends StatefulWidget {
   State<SignUp> createState() => _SignUpState();
 }
 
-class _SignUpState extends State<SignUp> {
+class _SignUpState extends State<SignUp> with TickerProviderStateMixin{
+
+  final int itemCount = 10;
+  late List<AnimationController>_controllers;
+  late List<Animation<Offset>> _animations;
   bool val = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _controllers = List.generate(
+        itemCount,
+      (index){
+          return AnimationController(
+            vsync: this,
+            duration: Duration(milliseconds: 500)
+          );
+      },
+    );
+    _animations = _controllers.map((controller){
+      return Tween<Offset>(
+        begin: Offset(0, -1),
+        end: Offset(0, 0)
+      ).animate(controller);
+    } ).toList();
+    _staggeredAnimation();
+  }
+
+  void _staggeredAnimation()async{
+    for(int i = _controllers.length-1; i>=0; i--){
+      await Future.delayed(Duration(milliseconds: 100));
+      _controllers[i].forward();
+    }
+  }
+
+  @override
+  void dispose() {
+    for(int i=0; i<_controllers.length; i++){
+      _controllers[i].dispose();
+    }
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,94 +70,140 @@ class _SignUpState extends State<SignUp> {
             child: Column(
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child:
-                        Text("Create an account", style: AppTextStyles.createAcc),
-                  ),
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: Text(
-                      AppStrings.help,
-                      style: AppTextStyles.helpStyle,
+
+                  SlideTransition(
+                    position: _animations[0],
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child:
+                          Text("Create an account", style: AppTextStyles.createAcc),
                     ),
                   ),
-                  AppDimens.s36,
-                  MainTextField(
-                    label: AppStrings.firstName,
-                    hint: AppStrings.enterFirst,
+
+                  SlideTransition(
+                    position: _animations[1],
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        AppStrings.help,
+                        style: AppTextStyles.helpStyle,
+                      ),
+                    ),
                   ),
+
                   AppDimens.s36,
-                  MainTextField(
-                    label: AppStrings.lastName,
-                    hint: AppStrings.enterLast,
+
+                  SlideTransition(
+                    position: _animations[2],
+                    child: MainTextField(
+                      label: AppStrings.firstName,
+                      hint: AppStrings.enterFirst,
+                    ),
                   ),
+
                   AppDimens.s36,
-                  MainTextField(
-                    label: AppStrings.phone,
-                    hint: "+998",
+
+                  SlideTransition(
+                    position: _animations[3],
+                    child: MainTextField(
+                      label: AppStrings.lastName,
+                      hint: AppStrings.enterLast,
+                    ),
                   ),
+
+                  AppDimens.s36,
+
+                  SlideTransition(
+                    position: _animations[4],
+                    child: MainTextField(
+                      label: AppStrings.phone,
+                      hint: "+998",
+                    ),
+                  ),
+
                   AppDimens.s24,
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Checkbox(
-                        checkColor: AppColors.white,
-                        activeColor: AppColors.green,
-                        value: val,
-                        onChanged: (value) {
-                          setState(() {
-                            val = value!;
-                          });
+
+                  SlideTransition(
+                    position: _animations[5],
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Checkbox(
+                          checkColor: AppColors.white,
+                          activeColor: AppColors.green,
+                          value: val,
+                          onChanged: (value) {
+                            setState(() {
+                              val = value!;
+                            });
+                          },
+                        ),
+                        Text(
+                          AppStrings.acceptTerms,
+                          style: AppTextStyles.acceptStyle,
+                        ),
+                      ],
+                    ),
+                  ),
+
+                  AppDimens.s24,
+
+                  SlideTransition(
+                    position: _animations[6],
+                    child: SizedBox(
+                      height: 60,
+                      width: double.infinity,
+                      child: MainElevatedButton(
+                        text: AppStrings.signUp,
+                        onTapped: (){
                         },
                       ),
-                      Text(
-                        AppStrings.acceptTerms,
-                        style: AppTextStyles.acceptStyle,
-                      ),
-                    ],
+                    ),
                   ),
-                  AppDimens.s24,
-                  SizedBox(
-                    height: 60,
-                    width: double.infinity,
-                    child: MainElevatedButton(
-                      text: AppStrings.signUp,
-                      onTapped: (){
 
-                      },
-                    ),
+                  SizedBox(height: 20),
+
+                  SlideTransition(
+                    position: _animations[7],
+                      child: OrSignInWith(),
                   ),
                   SizedBox(height: 20),
-                  OrSignInWith(),
-                  SizedBox(height: 20),
-                  SizedBox(
-                    width: 55,
-                    height: 55,
-                    child: Material(
-                      color: AppColors.white,
-                      elevation: 4,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(12)),
-                      child: AppIcons.googleBut,
+
+                  SlideTransition(
+                    position: _animations[8],
+                    child: SizedBox(
+                      width: 55,
+                      height: 55,
+                      child: Material(
+                        color: AppColors.white,
+                        elevation: 4,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12)),
+                        child: AppIcons.googleBut,
+                      ),
                     ),
                   ),
+
                   SizedBox(height: 36),
-                  RichText(
-                    text: TextSpan(
-                      children: [
-                        TextSpan(
-                            text: "Don't have an account?",
-                            style: AppTextStyles.dHAnAccount
-                        ),
-                        TextSpan(
-                            text: " Sign In",
-                            style: AppTextStyles.signInOut,
-                            recognizer: TapGestureRecognizer()
-                              ..onTap = (){
-                              }
-                        )
-                      ],
+
+                  SlideTransition(
+                    position: _animations[9],
+                    child: RichText(
+                      text: TextSpan(
+                        children: [
+                          TextSpan(
+                              text: "Don't have an account?",
+                              style: AppTextStyles.dHAnAccount
+                          ),
+                          TextSpan(
+                              text: " Sign In",
+                              style: AppTextStyles.signInOut,
+                              recognizer: TapGestureRecognizer()
+                                ..onTap = (){
+                                }
+                          )
+                        ],
+                      ),
                     ),
                   )
                 ],
