83fa135be202879cf6f86590c2b6e2d17a0ed5b0
MuhammadAli <mukhammadali@gmail.com>
2025-04-06 16:38:16 +0500

    version3 [keyngi page routesini berilar bolmasa xatolik bervoradi ]

--- lib/core/theme/dimens.dart
+++ lib/core/theme/dimens.dart
@@ -2,4 +2,9 @@ import 'package:flutter/material.dart';
 
 sealed class AppDimens{
   static SizedBox s24 = SizedBox(height: 24);
+
+  /// todo 8- page dimensi tegmanglar
+static const d54=54.0;
+static const d335=335.0;
+static const d12=12.0;
 }
--- lib/screen/page_eight/page_eight_components/custom_button.dart
+++ lib/screen/page_eight/page_eight_components/custom_button.dart
@@ -6,9 +6,10 @@ import '../../../core/theme/colors.dart';
 class CustomButton extends StatelessWidget {
   final String question;
   final String variant;
+  final void Function () onPressed; /// set State qilib keyngi page o'tish uchun
 
   const CustomButton(
-      {super.key, required this.question, required this.variant});
+      {super.key, required this.question, required this.variant, required this.onPressed});
 
   @override
   Widget build(BuildContext context) {
@@ -22,12 +23,12 @@ class CustomButton extends StatelessWidget {
         ),
       ),
       child: SizedBox(
-        height: 54,
-        width: 335,
+        height: AppDimens.d54,
+        width: AppDimens.d335,
         child: MaterialButton(
-          onPressed: () {},
+          onPressed: onPressed,
           shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(12),
+            borderRadius: BorderRadius.circular(AppDimens.d12),
           ),
           padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 20),
           child: RichText(
