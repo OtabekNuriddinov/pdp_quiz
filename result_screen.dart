128cc29a2355675b44311676f3cb750e4e773305
Abdusamatov Umidjon <umitjonumitjon690@gmail.com>
2025-04-07 10:19:44 +0500

    Add files via upload

---
+++ result_screen.dart
@@ -0,0 +1,253 @@
+
+import 'package:flutter/material.dart';
+
+class ResultPage extends StatefulWidget {
+  const ResultPage({super.key});
+
+  @override
+  State<ResultPage> createState() => _ResultPageState();
+}
+
+class _ResultPageState extends State<ResultPage> {
+
+  final PageController _pageController = PageController();
+  int _selectedIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 0,
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index;
+            _pageController.animateToPage(
+              index,
+              duration: const Duration(milliseconds: 300),
+              curve: Curves.easeInOut,
+            );
+          });
+        },
+        items:  [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Birinchi',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Ikkinchi',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Uchinchi',
+          ),
+        ],
+      ),
+      appBar: _buildAppBar(),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Stack(
+              // alignment: Alignment.center,
+              clipBehavior: Clip.none,
+              children: [
+                Container(
+                  height: 250,
+                  width: 600,
+                  decoration: BoxDecoration(
+                    color: Colors.green,
+                    borderRadius: BorderRadius.circular(14),
+                  ),
+                ),
+                Positioned(
+                  left: 190,
+                  top: 20,
+                  child: Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      Container(
+                        height: 120,
+                        width: 120,
+                        decoration: BoxDecoration(
+                          color: Colors.white.withAlpha(50),
+                          shape: BoxShape.circle,
+                        ),
+                      ),
+                      Container(
+                        height: 100,
+                        width: 100,
+                        decoration: BoxDecoration(
+                          color: Colors.white.withAlpha(100),
+                          shape: BoxShape.circle,
+                        ),
+                      ),
+                      Container(
+                        height: 80,
+                        width: 80,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          shape: BoxShape.circle,
+                        ),
+                        child: Center(
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: const [
+                              Text("Your",
+                                  style: TextStyle(color: Colors.green)),
+                              Text("Result",
+                                  style: TextStyle(color: Colors.green)),
+                              Text("15",
+                                  style: TextStyle(
+                                      color: Colors.green,
+                                      fontSize: 24,
+                                      fontWeight: FontWeight.bold)),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Positioned(
+                  top: 180,
+                  left: 45,
+                  child: Container(
+                    height: 150,
+                    width: 400,
+                    decoration: BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black,
+                          offset: Offset(0.1, 1),
+                          blurRadius: 1,
+                        ),
+                      ],
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Container(
+                              height: 20,
+                              width: 20,
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: Colors.green,
+                              ),
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            Text(
+                              "3 correct answers   ",
+                              style: TextStyle(fontSize: 20),
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Container(
+                              height: 20,
+                              width: 20,
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: Colors.red,
+                              ),
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            Text(
+                              "7 incorrect answers",
+                              style: TextStyle(fontSize: 20),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 170,
+            ),
+            myCustomButton(text: "Restart the test"),
+            SizedBox(height: 40,),
+            myCustomButton(text: "Return to main menu"),
+
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+AppBar _buildAppBar() {
+  return AppBar(
+    backgroundColor: Colors.white,
+    leading: IconButton(onPressed: () {}, icon: const Icon(Icons.arrow_back)),
+    title: const Text(
+      "Lorem ipsum dolor sit amet...",
+      style: TextStyle(fontFamily: "Poppins"),
+    ),
+    centerTitle: true,
+    actions: [
+      Padding(
+        padding: const EdgeInsets.only(right: 15.0),
+        child: CircleAvatar(
+          radius: 20,
+          child: Center(
+            child: Text("flutter iconi uchun joy"),
+          ),
+        ),
+      ),
+    ],
+  );
+}
+
+
+Container myCustomButton({required String text}){
+  return Container(
+    decoration: BoxDecoration(
+      color: Colors.white,
+      borderRadius: BorderRadius.circular(12),
+      border: Border.all(
+        color: Colors.green,
+        width: 2,
+      ),
+    ),
+    child: SizedBox(
+      height: 54,
+      width: 335,
+      child: MaterialButton(
+        onPressed: () {},
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12),
+        ),
+        padding:
+        const EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+        child: RichText(
+          text: TextSpan(
+            children: [
+              TextSpan(
+                  text: text,
+                  style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20)),
+              // TextSpan(text: "$question"),
+            ],
+          ),
+        ),
+      ),
+    ),
+  );
+}
